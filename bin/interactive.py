6a73744019744683f5bed976c292d43259a5a53b
RuanJohn <ruanjdekock@gmail.com>
2023-05-12 19:19:02 +0200

    feat: manual env imports for pickling

--- bin/interactive.py
+++ bin/interactive.py
@@ -5,7 +5,39 @@ import argparse
 
 from mpe.environment import MultiAgentEnv
 from mpe.policy import InteractivePolicy
-import mpe.scenarios as scenarios
+
+# Import all possible environments
+
+from mpe.scenarios.climbing_spread import Scenario as climbing_spread_scenario
+from mpe.scenarios.multi_speaker_listener import Scenario as multi_speaker_listener_scenario
+from mpe.scenarios.simple_adversary import Scenario as simple_adversary_scenario
+from mpe.scenarios.simple_crypto import Scenario as simple_crypto_scenario
+from mpe.scenarios.simple_doublespread import Scenario as simple_doublespread_scenario
+from mpe.scenarios.simple_push import Scenario as simple_push_scenario
+from mpe.scenarios.simple_reference import Scenario as simple_reference_scenario
+from mpe.scenarios.simple_speaker_listener import Scenario as simple_speaker_listener_scenario
+from mpe.scenarios.simple_spread import Scenario as simple_spread_scenario
+from mpe.scenarios.simple_tag import Scenario as simple_tag_scenario
+from mpe.scenarios.simple_world_comm import Scenario as simple_world_comm_scenario
+from mpe.scenarios.simple import Scenario as simple_scenario
+from mpe.scenarios.sparse_predator_prey import Scenario as sparse_predator_prey_scenario
+
+scenarios = {
+    'climbing_spread': climbing_spread_scenario,
+    'multi_speaker_listener': multi_speaker_listener_scenario,
+    'simple_adversary': simple_adversary_scenario,
+    'simple_crypto': simple_crypto_scenario,
+    'simple_doublespread': simple_doublespread_scenario,
+    'simple_push': simple_push_scenario,
+    'simple_reference': simple_reference_scenario,
+    'simple_speaker_listener': simple_speaker_listener_scenario,
+    'simple_spread': simple_spread_scenario,
+    'simple_tag': simple_tag_scenario,
+    'simple_world_comm': simple_world_comm_scenario,
+    'simple': simple_scenario,
+    'sparse_predator_prey': sparse_predator_prey_scenario,
+}
+
 
 if __name__ == '__main__':
     # parse arguments
@@ -14,7 +46,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # load scenario from script
-    scenario = scenarios.load(args.scenario).Scenario()
+    scenario_module = scenarios[args.scenario]
+    scenario = scenario_module()
     # create world
     world = scenario.make_world()
     # create multiagent environment
--- mpe/__init__.py
+++ mpe/__init__.py
@@ -1,5 +1,37 @@
 from gym.envs.registration import register
-import mpe.scenarios as scenarios
+
+# Import all possible environments
+
+from mpe.scenarios.climbing_spread import Scenario as climbing_spread_scenario
+from mpe.scenarios.multi_speaker_listener import Scenario as multi_speaker_listener_scenario
+from mpe.scenarios.simple_adversary import Scenario as simple_adversary_scenario
+from mpe.scenarios.simple_crypto import Scenario as simple_crypto_scenario
+from mpe.scenarios.simple_doublespread import Scenario as simple_doublespread_scenario
+from mpe.scenarios.simple_push import Scenario as simple_push_scenario
+from mpe.scenarios.simple_reference import Scenario as simple_reference_scenario
+from mpe.scenarios.simple_speaker_listener import Scenario as simple_speaker_listener_scenario
+from mpe.scenarios.simple_spread import Scenario as simple_spread_scenario
+from mpe.scenarios.simple_tag import Scenario as simple_tag_scenario
+from mpe.scenarios.simple_world_comm import Scenario as simple_world_comm_scenario
+from mpe.scenarios.simple import Scenario as simple_scenario
+from mpe.scenarios.sparse_predator_prey import Scenario as sparse_predator_prey_scenario
+
+scenarios = {
+    'climbing_spread': climbing_spread_scenario,
+    'multi_speaker_listener': multi_speaker_listener_scenario,
+    'simple_adversary': simple_adversary_scenario,
+    'simple_crypto': simple_crypto_scenario,
+    'simple_doublespread': simple_doublespread_scenario,
+    'simple_push': simple_push_scenario,
+    'simple_reference': simple_reference_scenario,
+    'simple_speaker_listener': simple_speaker_listener_scenario,
+    'simple_spread': simple_spread_scenario,
+    'simple_tag': simple_tag_scenario,
+    'simple_world_comm': simple_world_comm_scenario,
+    'simple': simple_scenario,
+    'sparse_predator_prey': sparse_predator_prey_scenario,
+}
+
 # Multiagent envs
 # ----------------------------------------
 
@@ -17,7 +49,8 @@ _particles = {
 }
 
 for scenario_name, gymkey in _particles.items():
-    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    scenario_module = scenarios[scenario_name]
+    scenario = scenario_module()
     world = scenario.make_world()
 
     # Registers multi-agent particle environments:
@@ -37,7 +70,8 @@ for scenario_name, gymkey in _particles.items():
 for N in range(2, 11, 2):
     scenario_name = "simple_doublespread"
     gymkey = f"DoubleSpread-{N}ag-v0"
-    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    scenario_module = scenarios[scenario_name]
+    scenario = scenario_module()
     world = scenario.make_world(N)
 
     register(
