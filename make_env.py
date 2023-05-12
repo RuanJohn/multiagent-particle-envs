04eb367da667ce78bc621d68d39555fc1b8cd6c5
RuanJohn <ruanjdekock@gmail.com>
2023-05-12 18:23:17 +0200

    feat: manual scenario imports

--- make_env.py
+++ make_env.py
@@ -1,3 +1,35 @@
+# Import all possible environments
+
+from mpe.scenarios.climbing_spread import Scenario as climbing_spread_scenario
+from mpe.scenarios.multi_speaker_listener import Scenario as multi_speaker_listener_scenario
+from mpe.scenarios.simple_adversary import Scenario as simple_adversary_scenario
+from mpe.scenarios.simple_crypto import Scenario as simple_crypto_scenario
+from mpe.scenarios.simple_doublespread import Scenario as simple_doublespread_scenario
+from mpe.scenarios.simple_push import Scenario as simple_push_scenario
+from mpe.scenarios.simple_reference import Scenario as simple_reference_scenario
+from mpe.scenarios.simple_speaker_listener import Scenario as simple_speaker_listener_scenario
+from mpe.scenarios.simple_spread import Scenario as simple_spread_scenario
+from mpe.scenarios.simple_tag import Scenario as simple_tag_scenario
+from mpe.scenarios.simple_world_comm import Scenario as simple_world_comm_scenario
+from mpe.scenarios.simple import Scenario as simple_scenario
+from mpe.scenarios.sparse_predator_prey import Scenario as sparse_predator_prey_scenario
+
+scenarios = {
+    'climbing_spread': climbing_spread_scenario,
+    'multi_speaker_listener': multi_speaker_listener_scenario,
+    'simple_adversary': simple_adversary_scenario,
+    'simple_crypto': simple_crypto_scenario,
+    'simple_doublespread': simple_doublespread_scenario,
+    'simple_push': simple_push_scenario,
+    'simple_reference': simple_reference_scenario,
+    'simple_speaker_listener': simple_speaker_listener_scenario,
+    'simple_spread': simple_spread_scenario,
+    'simple_tag': simple_tag_scenario,
+    'simple_world_comm': simple_world_comm_scenario,
+    'simple': simple_scenario,
+    'sparse_predator_prey': sparse_predator_prey_scenario,
+}
+
 """
 Code for creating a multiagent environment with one of the scenarios listed
 in ./scenarios/.
@@ -30,10 +62,10 @@ def make_env(scenario_name, benchmark=False):
         .n                  :   Returns the number of Agents
     '''
     from mpe.environment import MultiAgentEnv
-    import mpe.scenarios as scenarios
 
     # load scenario from script
-    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    scenario_module = scenarios[scenario_name]
+    scenario = scenario_module()
     # create world
     world = scenario.make_world()
     # create multiagent environment
